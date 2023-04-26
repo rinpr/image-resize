6e1cb934f61c2dd7029b1c4e76d64612e51a4fa6
rin <duggerseven@gmail.com>
2023-04-27 01:20:31 +0700

    update main func

--- main.go
+++ main.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"github.com/sunshineplan/imgconv"
-	"image-resize/config"
-	"log"
+	"image-resize/img"
+	"image-resize/jsonconfig"
 )
 
 func main() {
-	// Open a test image.
-	src, err := imgconv.Open("image/7.png")
-	if err != nil {
-		log.Fatalf("failed to open image: %v", err)
-	}
-
-	// Resize the image to width = 200px preserving the aspect ratio.
-	mark := imgconv.Resize(src, &imgconv.ResizeOption{Width: config.GetWidth()})
-
-	// Write the resulting image as PNG.
-	err = imgconv.Save("image/result.png", mark, &imgconv.FormatOption{Format: imgconv.PNG})
-	if err != nil {
-		log.Fatalf("failed to write image: %v", err)
-	}
-	fmt.Println("Successfully exported resized image!")
+	img.Resize(jsonconfig.GetConfig(0))
 }
