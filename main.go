9667fbd4fa2e1b1110f8b1e561c4900ddb93f9c4
rin <duggerseven@gmail.com>
2023-05-01 17:04:57 +0700

    concurrency on air?

--- main.go
+++ main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"fmt"
 	"image-resize/img"
+	"sync"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello World!")
+
+	start := time.Now()
+
+	// Call the function you want to time
+	resizeAllfaster()
+
+	end := time.Now()
+	duration := end.Sub(start)
+
+	fmt.Printf("Function took %v milliseconds to execute\n", duration.Milliseconds())
+
 }
 
 func resizeSingleImage(file string) {
@@ -21,4 +34,25 @@ func resizeMultipleImage() {
 	for _, filename := range file {
 		img.Resize(filename)
 	}
+}
+
+func resizeAllfaster() {
+	// Concurrency video: https://www.youtube.com/watch?v=_uk9BN3a0eo
+
+	file, err := img.GetImageFile()
+	if err != nil {
+		return
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(file))
+
+	for _, filename := range file {
+		go func(filename string) {
+			defer wg.Done()
+			img.Resize(filename)
+		}(filename)
+	}
+
+	wg.Wait()
 }
